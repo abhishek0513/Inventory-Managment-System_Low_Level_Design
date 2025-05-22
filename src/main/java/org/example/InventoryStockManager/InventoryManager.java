f041c1997d63c5bda6c5af623212906d6d944aec
Abhishek Mishra <abhishekmishra0513@gmail.com>
2025-05-22 22:06:30 +0530

    commit 02 - 7% done

--- src/main/java/org/example/InventoryStockManager/InventoryManager.java
+++ src/main/java/org/example/InventoryStockManager/InventoryManager.java
@@ -1,4 +1,20 @@
 package org.example.InventoryStockManager;
 
+import org.example.UtilityClasses.Warehouse;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InventoryManager {
+    private static InventoryManager instance;
+    private List<Warehouse> warehouses;
+    private ProductFactory productFactory;
+    private ReplenishmentStrategy replenishmentStrategy;
+
+    private InventoryManager(ReplenishmentStrategy replenishmentStrategy){
+        warehouses = new ArrayList<>();
+        productFactory = new ProductFactory();
+        this.replenishmentStrategy = replenishmentStrategy;
+    }
+    //SingleTon design pattern;
 }
